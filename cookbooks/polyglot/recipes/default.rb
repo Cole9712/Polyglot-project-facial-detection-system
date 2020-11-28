0870a340afc98e923bc8461a4b62e614c664475f
Hao Wu <hwa129@sfu.ca>
2020-11-28 00:38:34 -0800

    change cookbook

--- cookbooks/polyglot/recipes/default.rb
+++ cookbooks/polyglot/recipes/default.rb
@@ -4,7 +4,7 @@ ubuntu_release = 'focal'
 ubuntu_version = '20.04'
 username = 'vagrant'
 user_home = '/home/' + username
-project_home = user_home + '/project/mq-demos' # you may need to change the working directory to match your project
+project_home = user_home + '/project/' # you may need to change the working directory to match your project
 
 
 python3_packages = '/usr/local/lib/python3.8/dist-packages'
@@ -33,14 +33,14 @@ directory '/opt/installers'
 
 # Basic packages many of us probably want. Includes gcc C and C++ compilers.
 
-#package ['build-essential', 'cmake']
+package ['build-essential', 'cmake']
 
 
 # Other core language tools you might want
 
-#package ['python3', 'python3-pip', 'python3-dev']  # Python
+package ['python3', 'python3-pip', 'python3-dev']  # Python
 #package ['ghc', 'libghc-random-dev', 'cabal-install']  # Haskell
-#package 'golang-go'  # Go
+# package 'golang-go'  # Go
 #package 'erlang'  # Erlang
 #package 'ocaml-nox'  # OCaml
 #package ['rustc', 'cargo']  # Rust
@@ -88,21 +88,21 @@ directory '/opt/installers'
 
 # NodeJS (more modern than Ubuntu nodejs package) and NPM
 
-#remote_file '/opt/installers/node-setup.sh' do
-#  source 'https://deb.nodesource.com/setup_14.x'
-#  mode '0755'
-#end
-#execute '/opt/installers/node-setup.sh' do
-#  creates '/etc/apt/sources.list.d/nodesource.list'
-#  notifies :run, 'execute[apt-get update]', :immediately
-#end
-#package ['nodejs']
+remote_file '/opt/installers/node-setup.sh' do
+ source 'https://deb.nodesource.com/setup_14.x'
+ mode '0755'
+end
+execute '/opt/installers/node-setup.sh' do
+ creates '/etc/apt/sources.list.d/nodesource.list'
+ notifies :run, 'execute[apt-get update]', :immediately
+end
+package ['nodejs']
 
 
 # Go (more modern than Ubuntu golang-go package)
 
-#execute 'snap install --classic go' do
-#end
+execute 'snap install --classic go' do
+end
 
 # SWIG
 
