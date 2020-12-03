e844d3e370f8dcb2e7619d8b906d40504ad0ff4e
hwa129 <hwa129@sfu.ca>
2020-12-03 12:58:18 +0000

    change proxy config

--- client/vue.config.js
+++ client/vue.config.js
@@ -2,7 +2,7 @@ module.exports={
     devServer: {
         proxy: {
             '/uploadMine': {
-                target: 'http://127.0.0.1:8082'
+                target: 'http://127.0.0.1:8082/'
                 // pathRewrite: {'^/api' : ''}
             }
         }
