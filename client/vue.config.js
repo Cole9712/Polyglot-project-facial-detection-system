9deab406f40cef637f4f293bcd1f58b6304b16fe
hwa129 <hwa129@sfu.ca>
2020-12-04 09:08:23 +0000

    change proxy settings

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,14 +1,16 @@
 module.exports={
     devServer: {
-        // proxy: {
-        //     '^/uploadMine': {
-        //         target: 'http://127.0.0.1:8082',
-        //         secure: false,
-        //         ws: true,
-        //         changeOrigin:true
-        //         // pathRewrite: {'^/api' : ''}
-        //     }
-        // }
-        proxy: 'http://127.0.0.1:8082'
+        proxy: {
+            '^/uploadMine': {
+                target: 'http://127.0.0.1:8082/',
+                secure: false,
+                ws: true,
+                changeOrigin:true,
+                headers: {
+                    Connection: 'keep-alive'
+                }
+                // pathRewrite: {'^/api' : ''}
+            }
+        }
     }
 }
