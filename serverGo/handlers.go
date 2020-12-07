46e74041b2d470f8cca67f86e51b7aaced5cd5ec
Hao Wu <hwa129@sfu.ca>
2020-12-06 23:50:41 -0800

    adding back-end service for facial detect request

--- serverGo/handlers.go
+++ serverGo/handlers.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"os"
@@ -19,6 +21,55 @@ type Response struct {
 type SwapRequest struct {
 	File1 string `json:"file1"`
 	File2 string `json:"file2"`
+}
+
+type HomeFacialRequest struct {
+	ImgUrl string `json:"imgUrl"`
+	ID     string `json:"id"`
+	Ext    string `json:"ext"`
+}
+
+func homeFacialHandler(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	// save file from request
+	var hfr HomeFacialRequest
+	err := json.NewDecoder(r.Body).Decode(&hfr)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	response, e := http.Get(hfr.ImgUrl)
+	if e != nil {
+		log.Fatal(e)
+	}
+	defer response.Body.Close()
+	path := "./homeOutput/" + hfr.ID + "." + hfr.Ext
+	f1, _ := os.OpenFile(path, os.O_WRONLY|os.O_CREATE, 0666)
+	defer f1.Close()
+
+	_, err = io.Copy(f1, response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Save File Successfully!")
+	cmd := exec.Command("python3", "main.py", "detectAPI", hfr.ID+"."+hfr.Ext)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err = cmd.Run()
+	if err != nil {
+		log.Fatal("cmd Run failed")
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	returnResponse := Response{true, nil, "http://127.0.0.1:8082/" + hfr.ID + "." + hfr.Ext}
+	w.Header().Set("Content-Type", "application/json")
+	err = json.NewEncoder(w).Encode(returnResponse)
+	if err != nil {
+		panic(err)
+	}
+	w.WriteHeader(http.StatusOK)
+	log.Println("Response sent with StatusOK")
+
 }
 
 func swapRequestHandler(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
@@ -40,7 +91,7 @@ func swapRequestHandler(w http.ResponseWriter, r *http.Request, p httprouter.Par
 
 	response := Response{true, nil, "http://127.0.0.1:8082/" + sr.File1}
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(response)
+	err = json.NewEncoder(w).Encode(response)
 	if err != nil {
 		panic(err)
 	}
--- serverGo/main.go
+++ serverGo/main.go
@@ -16,7 +16,9 @@ func main() {
 	// router.GET("/", indexHandler(router))
 	router.POST("/uploadMinePost", fileUploadHandler)
 	router.POST("/uploadMinePost/swap", swapRequestHandler)
+	router.POST("/home", homeFacialHandler)
 	router.ServeFiles("/*filepath", http.Dir("./facialOutput"))
+	// router.ServeFiles("/homeOutput/*filepath", http.Dir("./homeOutput"))
 	corsHandler := cors.Default().Handler(router)
 	log.Fatal(http.ListenAndServe("0.0.0.0:8082", corsHandler))
 }
