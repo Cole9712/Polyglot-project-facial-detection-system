6b9a742b8bbb3dc05ca91246f62fb8e561316b73
Hao Wu <hwa129@sfu.ca>
2020-12-06 18:34:11 -0800

    update proxy

--- client/vue.config.js
+++ client/vue.config.js
@@ -20,7 +20,17 @@ module.exports={
                     Connection: 'keep-alive'
                 }
                 // pathRewrite: {'^/api' : ''}
-            }
+            },
+            '^/home': {
+                target: 'http://127.0.0.1:8082/',
+                secure: false,
+                ws: true,
+                changeOrigin:true,
+                headers: {
+                    Connection: 'keep-alive'
+                }
+                // pathRewrite: {'^/api' : ''}
+            },
         }
     }
 }
