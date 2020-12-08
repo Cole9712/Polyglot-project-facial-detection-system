299f1d7b8cec67968a172b72bca482d8c5e816b0
Hao Wu <hwa129@sfu.ca>
2020-12-08 00:46:23 -0800

    adding randomNumberGenerator helper func

--- serverGo/helpers.go
+++ serverGo/helpers.go
@@ -1,26 +1,18 @@
 package main
 
 import (
-	"encoding/base64"
-	"encoding/json"
-	"fmt"
-	"image"
-	"image/jpeg"
 	"io"
 	"log"
+	"math/rand"
 	"net/http"
 	"os"
-	"strings"
+	"strconv"
+	"time"
 )
-
-type imageGot struct {
-	ImageType   string `json:"ImageType"`
-	ImageBase64 string `json:"ImageBase64"`
-}
 
 func saveFile(r *http.Request, fileName string) (bool, error, string) {
 	log.Println("Start Processing file uploading. Received File name:" + fileName)
-	
+
 	f1, handler, err := r.FormFile(fileName)
 	if err != nil {
 		log.Println("Error1")
@@ -28,7 +20,7 @@ func saveFile(r *http.Request, fileName string) (bool, error, string) {
 	}
 	defer f1.Close()
 
-	path := "./output/"+handler.Filename
+	path := "./output/" + handler.Filename
 	f2, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE, 0666)
 	if err != nil {
 		log.Println("Error2")
@@ -46,49 +38,7 @@ func saveFile(r *http.Request, fileName string) (bool, error, string) {
 	return true, nil, handler.Filename
 }
 
-func parseImageJSON(r *http.Request, w http.ResponseWriter) (imageGot, error) {
-	// declare a received image struct type
-	var receivedImage imageGot
-
-	err := json.NewDecoder(r.Body).Decode(&receivedImage)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-
-	}
-
-	return receivedImage, err
-}
-
-// Save a JPG to ./output/
-func saveJPG(input image.Image) string {
-	fileName := "/output/tempPic.jpg"
-	f, err := os.OpenFile(fileName, os.O_WRONLY|os.O_CREATE, 0777)
-	if err != nil {
-		log.Fatal(err)
-		return "error: saveJPG openFile"
-	}
-
-	err = jpeg.Encode(f, input, &jpeg.Options{Quality: 100})
-	if err != nil {
-		log.Fatal(err)
-		return "error: saveJPG jpeg.Encode"
-	}
-
-	fmt.Println("JPG File created!")
-	return "success!"
-}
-
-// adapted from https://github.com/rravishankar/golangtraining/tree/master/test/jpegencode
-// convert base64 string to image file (JPG)
-func saveBase64(input string, format string) string {
-	reader := base64.NewDecoder(base64.StdEncoding, strings.NewReader(input))
-	i, _, err := image.Decode(reader)
-	if err != nil {
-		log.Fatal(err)
-		return "error: saveBase64"
-	}
-
-	message := saveJPG(i)
-
-	return message
+func randomParamGen() string {
+	gen := rand.New(rand.NewSource(time.Now().UnixNano()))
+	return "random=" + strconv.Itoa(gen.Intn(3000000))
 }
